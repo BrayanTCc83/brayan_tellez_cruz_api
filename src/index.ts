052f6f9d34eb1b27fa6952b4b6b7d5fcaea9bfb8
Brayan <brayan.tellez.cruz.000@gmail.com>
2025-02-27 14:41:26 -0600

    Agregando base de datos y api de información personal

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { APP, PORT } from "./api/APP";
 import cors from "cors";
+import express from "express";
 import BuildBlog from "./api/blog";
 import BuildProject from "./api/project";
+import BuildRoot from "./api/root";
 
 APP.use(cors({
     credentials: true
 }));
+APP.use('/static', express.static('public'));
 
+BuildRoot();
 BuildBlog();
 BuildProject();
 
---
+++ src/settings/routes.ts
@@ -0,0 +1,24 @@
+export const PROJECT_URL = {
+    projects: '/projects',
+    project: '/projects/:projectId',
+    images: '/projects/:projectId/images',
+    comments: '/projects/:projectId/comments'
+};
+
+export const BLOG_URL = {
+    blogs: '/blogs',
+    blog: '/blogs/:blogId',
+    comments: '/blogs/:blogId/comments'
+};
+
+export const INDEX_URL = {
+    about: '/about',
+    mainproject: '/mainproject',
+    images: '/mainproject/images'
+};
+
+export const USER_URL = {
+    login: '/login',
+    register: '/register',
+    users: '/users'
+};
